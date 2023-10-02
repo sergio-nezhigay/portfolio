d915999db62200c6d48552e602bf625cc485e0fb
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-02 13:41:02 +0300

    button accassability update

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -9,8 +9,14 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 focus:ring-2 focus:ring-blue-500 focus:outline-none"
       onClick={toggleTheme}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          toggleTheme();
+        }
+      }}
+      aria-label={`Toggle ${theme === "light" ? "Dark" : "Light"} Mode`}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
