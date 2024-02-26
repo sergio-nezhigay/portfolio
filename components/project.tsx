5b0422990ce3e070b5fe741601e56268c93bbd36
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-26 21:19:37 +0200

    color change

--- components/project.tsx
+++ components/project.tsx
@@ -36,7 +36,7 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-300 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -52,13 +52,13 @@ export default function Project({
               </li>
             ))}
           </ul>
-          <div className="mt-4 sm:mt-auto flex gap-2">
+          <div className="mt-4 sm:mt-auto flex gap-3">
             {github && (
               <a
                 href={github}
                 rel="nofollow noreferrer noopener"
                 target="_blank"
-                className="bg-white text-black/[0.7] hover:text-black py-0.5 px-3 rounded-full outline-none flex 
+                className="bg-white text-black/[0.8] hover:text-black py-0.5 px-3 rounded-full outline-none flex
               items-center focus:scale-110 hover:scale-110  active:scale-105 transition"
               >
                 <AiFillGithub className="mr-2" /> GitHub
@@ -69,7 +69,7 @@ export default function Project({
                 href={live}
                 rel="nofollow noreferrer noopener"
                 target="_blank"
-                className="bg-white text-black/[0.7] hover:text-black py-0.5 px-3 rounded-full outline-none flex 
+                className="bg-white text-black/[0.7] hover:text-black py-0.5 px-3 rounded-full outline-none flex
               items-center focus:scale-110 hover:scale-110  active:scale-105 transition"
               >
                 <FaGlobe className="mr-2" /> Live page
@@ -87,7 +87,7 @@ export default function Project({
             convertImage(96, 96, BLUR_COLOR)
           )}`}
           className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        transition 
+        transition
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
         group-hover:translate-y-3
