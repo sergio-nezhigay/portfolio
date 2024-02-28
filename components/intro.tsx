5d00e17cdd9ff38a3003193d2061a04e143465c3
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-28 18:13:23 +0200

    add +

--- components/intro.tsx
+++ components/intro.tsx
@@ -70,7 +70,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Serhii.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
+        <span className="font-bold">2+ years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
