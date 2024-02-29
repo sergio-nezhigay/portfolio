6000b31868b8317593ec923f3b5506bb3118620b
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-29 16:02:09 +0200

    change link to cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -95,7 +95,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="https://rxresu.me/sergio8655/wsery6-tewr"
           download
         >
           Download CV
